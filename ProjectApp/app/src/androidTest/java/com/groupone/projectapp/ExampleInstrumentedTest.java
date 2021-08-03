9efd2f077a4ea4e8180cf406fd68bb5caf2b49de
XZY23333 <kelvin.xiang@mail.utoronto.ca>
2021-08-03 17:31:33 -0400

    change template to empty activity

--- ProjectApp/app/src/androidTest/java/com/groupone/projectapp/ExampleInstrumentedTest.java
+++ ProjectApp/app/src/androidTest/java/com/groupone/projectapp/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.projectapp;
+package com.groupone.projectapp;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.projectapp", appContext.getPackageName());
+        assertEquals("com.groupone.projectapp", appContext.getPackageName());
     }
 }
