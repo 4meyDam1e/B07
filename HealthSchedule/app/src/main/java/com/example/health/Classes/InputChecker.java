5ee7476d40698d00457bab6c4df7815520b9213a
Destructa321 <amey.damle@mail.utoronto.ca>
2021-08-08 14:15:30 -0400

    finished InputChecker Class

--- HealthSchedule/app/src/main/java/com/example/health/Classes/InputChecker.java
+++ HealthSchedule/app/src/main/java/com/example/health/Classes/InputChecker.java
@@ -1,14 +1,20 @@
 package com.example.health.Classes;
 
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputChecker {
     public static boolean checkEmail(String email) {
-        if (email.length() < 1) return false;
-        return true;
+        Pattern emailPattern = Pattern.compile("([[a-z][A-Z][0-9]._])+@([a-z])+.com");
+        Matcher emailMatcher = emailPattern.matcher(email);
+        return emailMatcher.matches();
     }
 
     public static boolean checkPassword(String password) {
-        if (password.length() < 6) return false;
-        return true;
+        Pattern passwordPattern = Pattern.compile("([[a-z][A-Z][0-9]]){6,}");
+        Matcher passwordMatcher = passwordPattern.matcher(password);
+        return passwordMatcher.matches();
     }
 
     public static boolean checkName(String name) {
@@ -17,13 +23,36 @@ public class InputChecker {
     }
 
     public static boolean checkBirthday(String birthday) {
-        if (birthday.length() < 1) return false;
-        return true;
+        HashMap<String, Integer> monthToDays = new HashMap<String, Integer>();
+        monthToDays.put("01", 31);
+        monthToDays.put("02", 29);
+        monthToDays.put("03", 31);
+        monthToDays.put("04", 30);
+        monthToDays.put("05", 31);
+        monthToDays.put("06", 30);
+        monthToDays.put("07", 31);
+        monthToDays.put("08", 31);
+        monthToDays.put("09", 30);
+        monthToDays.put("10", 31);
+        monthToDays.put("11", 30);
+        monthToDays.put("12", 31);
+
+        Pattern birthdayPattern = Pattern.compile("[0-9]{4}[0-9]{2}[0-9]{2}");
+        Matcher birthdayMatcher = birthdayPattern.matcher(birthday);
+        //If the format is right...
+        if (birthdayMatcher.matches()) {
+            //IGNORE year
+            //If the number of days in the month are realistic (IGNORING non-leap year, assuming February has 29)
+            if (Integer.parseInt(birthday.substring(8)) <= monthToDays.get(birthday.substring(5,7)))
+                return true;
+        }
+        return false;
     }
 
     public static boolean checkHealthCard(String healthCard) {
-        if (healthCard.length() < 1) return false;
-        return true;
+        Pattern healthCardPattern = Pattern.compile("([0-9]){10}");
+        Matcher healthCardMatcher = healthCardPattern.matcher(healthCard);
+        return healthCardMatcher.matches();
     }
 
     public static boolean checkProficiency(String proficiency) {
