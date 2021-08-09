4550dc401dbb6f501f65c683ac5ea4f3ecff0e7f
Destructa321 <amey.damle@mail.utoronto.ca>
2021-08-09 14:36:57 -0400

    Added tests

--- HealthSchedule/app/src/test/java/com/example/health/PresenterUnitTests.java
+++ HealthSchedule/app/src/test/java/com/example/health/PresenterUnitTests.java
@@ -9,27 +9,59 @@ import org.mockito.MockedStatic;
 import static org.mockito.Mockito.*;
 
 import com.example.health.Classes.InputChecker;
+import com.example.health.Classes.User;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class PresenterUnitTests {
     private MainActivity mockMainActivity = mock(MainActivity.class);
+    //Create a Presenter using our mockMainAcitivity.
     private Presenter presenter = new Presenter(mockMainActivity);
-//    private InputChecker mockInputChecker = mock(InputChecker.class);
 
     @Before
     public void setUp() {
+        //Force out mockMainActivity to spit out the following.
         when(mockMainActivity.getEmail()).thenReturn("mock@email.com");
         when(mockMainActivity.getPassword()).thenReturn("mockPassword");
     }
-
+    //-----------------------------------------------------------------------------------------------------------------------
     @Test
     public void testAttemptLoginWrongEmailFormat() {
-        when(InputChecker.checkEmail("mock@email.com")).thenReturn(false);
-        when(InputChecker.checkPassword("mockPassword")).thenReturn(true);
-        presenter.attemptLogin();
+        try (MockedStatic<InputChecker> mockInputChecker = mockStatic(InputChecker.class)) {
+            //Force InputChecker to return the following when
+            // given the specific email and password (which were also fixed by us in mockMainActivity).
+            mockInputChecker.when(() -> InputChecker.checkEmail("mock@email.com")).thenReturn(false);
+            mockInputChecker.when(() -> InputChecker.checkEmail("mockPassword")).thenReturn(true);
 
-        try (MockedStatic<InputChecker> utilities = mockStatic(InputChecker.class)) {
-
+            presenter.attemptLogin();
+            verify(mockMainActivity, times(1)).showMessage("The input format is wrong!");
         }
-        verify(mockMainActivity, times(1)).showMessage("The input format is wrong!");
+    }
+
+    @Test
+    public void testAttemptLoginWrongPasswordFormat() {
+        try (MockedStatic<InputChecker> mockInputChecker = mockStatic(InputChecker.class)) {
+            //Force InputChecker to return the following when
+            // given the specific email and password (which were also fixed by us in mockMainActivity).
+            mockInputChecker.when(() -> InputChecker.checkEmail("mock@email.com")).thenReturn(true);
+            mockInputChecker.when(() -> InputChecker.checkEmail("mockPassword")).thenReturn(false);
+
+            presenter.attemptLogin();
+            verify(mockMainActivity, times(1)).showMessage("The input format is wrong!");
+        }
+    }
+    //--------------------------------------------------------------------------------------------------------------------
+    @Test
+    public void testAttemptLoginWrongEmail() {
+        DatabaseReference mockDBRef = mock(DatabaseReference.class);
+
+        try (MockedStatic<FirebaseDatabase> mockFirebaseDatabase = mockStatic(FirebaseDatabase.class)) {
+            mockFirebaseDatabase.when(() -> FirebaseDatabase.getInstance().getReference()
+                            .child("Users").child(User.getID("mock@email.com"))).thenReturn(mockDBRef);
+
+            mockDBRef.removeValue();
+            presenter.attemptLogin();
+            verify(mockMainActivity, times(1)).showMessage("The input format is wrong!");
+        }
     }
 }
